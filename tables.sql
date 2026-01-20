8d6f62a5c9778b4702c1d763d8e25153688bd42c
PabloGartzi <pablogartzi02@gmail.com>
2026-01-20 11:11:22 +0100

    Actualicación de las querys de la base de datos

--- tables.sql
+++ tables.sql
@@ -1,8 +1,38 @@
-DROP TABLE IF EXISTS users;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE roles (
+    uid_rol UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    rol_type VARCHAR(50) NOT NULL UNIQUE
+);
+
+INSERT INTO roles (rol_type) VALUES
+('productor'),
+('distribuidor'),
+('asesor'),
+('analista'),
+('director');
+---
 
 CREATE TABLE users (
-    firebase_uid_user VARCHAR(255) UNIQUE NOT NULL,
+    firebase_uid_user VARCHAR(255) PRIMARY KEY,
     name_user VARCHAR(100),
     email_user VARCHAR(255) UNIQUE NOT NULL,
-    uid_role_user INT
-)
+    uid_rol UUID NOT NULL REFERENCES roles(uid_rol)
+);
+---
+
+CREATE TABLE director_producer (
+    uid_director VARCHAR(255) NOT NULL REFERENCES users(firebase_uid_user),
+    uid_producer VARCHAR(255) NOT NULL REFERENCES users(firebase_uid_user),
+    PRIMARY KEY (uid_director, uid_producer)
+);
+---
+
+CREATE TABLE parcels (
+    uid_parcel VARCHAR(255) PRIMARY KEY,
+    uid_producer VARCHAR(255) NOT NULL REFERENCES users(firebase_uid_user),
+    name_parcel VARCHAR(100),
+    product_parcel VARCHAR(100),
+    coordinates_parcel TEXT
+);
+---
